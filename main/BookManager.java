f914a3e130557cdd42e8db0da7698550cbabd76e
이찬희 <cksgml019@naver.com>
2024-06-12 18:41:29 +0900

    [FEATURE] BookManager 클래스 매서드 최종 구현

--- main/BookManager.java
+++ main/BookManager.java
@@ -16,10 +16,30 @@ public class BookManager {
             if (b.getId() == book.getId()) {
                 throw new IllegalArgumentException("해당 ID(" + book.getId() + ") 는 이미 존재합니다.\n");
             }
-
         }
         books.add(book);
         System.out.println(book.toString() + "도서가 추가되었습니다.\n");
     }
 
+    public void searchBook(Book book){
+
+        for (Book b : books) {
+            if (b.getId() == book.getId()) {
+                System.out.println("검색결과: \n");
+                System.out.println(book.toString() + '\n');
+                return;
+            }
+        }
+        System.out.println("검색된 도서가 없습니다.\n");
+    }
+
+    public void removeBook(Book book){
+
+        boolean removed = books.removeIf(b -> b.getId() == book.getId());
+        if (removed) {
+            System.out.println(book.toString() + "도서를 삭제하였습니다.\n");
+        } else {
+            System.out.println("해당 ID( "+ book.getId() + ")의 도서를 찾을 수 없습니다.\n");
+        }
+    }
 }
